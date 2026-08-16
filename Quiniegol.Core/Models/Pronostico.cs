e5df89c98f36f079bfca3441bd1b45b6ec394479
Tayron Salas <tayronsalas@gmail.com>
2026-08-16 00:05:38 -0600

    Agrega proyecto web Quiniegol

---
+++ Quiniegol.Core/Models/Pronostico.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiniegol.Core.Models
+{
+    public class Pronostico
+    {
+        public int IdPronostico { get; set; }
+
+        public string IdEmpleado { get; set; }
+
+        public int IdPartido { get; set; }
+
+        public string ResultadoPronosticado { get; set; }
+    }
+}
